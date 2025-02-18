c9628a6aa0ae8f0fe7e456c233ef7ba9e6e52a01
srshakil2 <rh3642921@gmail.com>
2025-02-19 02:37:56 +0600

    landing and all class pages sorting done

--- src/Components/AllClass/AllClass.jsx
+++ src/Components/AllClass/AllClass.jsx
@@ -5,6 +5,7 @@ import { Helmet } from "react-helmet";
 
 const AllClass = () => {
   const [allClass, setAllClass] = useState([]);
+  const [sorted, setSorted] = useState("");
   const [data, refetch] = useNavHome();
   //   console.log(allClass);
   useEffect(() => {
@@ -12,56 +13,81 @@ const AllClass = () => {
       const items = item?.status === "approves";
       return items;
     });
+
+    if (sorted == "asc") {
+      const ascSort = filteringData.sort((a, b) => a.enroll - b.enroll);
+      setAllClass(ascSort);
+      return;
+    } else if (sorted == "desc") {
+      const descSort = filteringData.sort((a, b) => b.enroll - a.enroll);
+
+      setAllClass(descSort);
+      return;
+    }
+
     setAllClass(filteringData);
     refetch();
-  }, [data]);
+  }, [data, sorted]);
 
   return (
-    <div className="w-11/12 py-10 mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+    <>
       <Helmet>
         <title>Education || Allclass</title>
       </Helmet>
-      {allClass.map((dataItem, i) => (
-        <div key={i}>
-          <div className="card bg-base-100 shadow-xl ">
-            <figure>
-              <img
-                src={dataItem?.photoUrl}
-                alt={dataItem?.title}
-                className="h-48 w-full object-cover"
-              />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title text-lg font-bold">
-                {dataItem?.title}
-              </h2>
-              <p className="text-sm text-gray-600">
-                Posted by: {dataItem?.name}
-              </p>
-              <p className="text-sm text-gray-600">
-                {dataItem?.bio.slice(1, 100)}...
-              </p>
-              <div className="flex justify-between items-center mt-4">
-                <span className="text-lg font-semibold text-green-600">
-                  Price : ${dataItem?.price}
-                </span>
-                <span className="text-sm text-gray-500">
-                  Enrolled: {dataItem?.enroll}
-                </span>
+      <div className="flex gap-3 items-center justify-center mb-6 mt-10">
+        <h4 className="text-xl font-semibold text-nowrap">Sort By Enroll:</h4>
+        <select
+          onChange={(e) => setSorted(e.target.value)}
+          className="select select-bordered w-full max-w-xs"
+        >
+          <option value={""}>Default Data</option>
+          <option value="asc">Ascending By Enroll</option>
+          <option value="desc">Descending By Enroll</option>
+        </select>
+      </div>
+      <div className="w-11/12  mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-10">
+        {allClass.map((dataItem, i) => (
+          <div key={i}>
+            <div className="card bg-base-100 shadow-xl ">
+              <figure>
+                <img
+                  src={dataItem?.photoUrl}
+                  alt={dataItem?.title}
+                  className="h-48 w-full object-cover"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title text-lg font-bold">
+                  {dataItem?.title}
+                </h2>
+                <p className="text-sm text-gray-600">
+                  Posted by: {dataItem?.name}
+                </p>
+                <p className="text-sm text-gray-600">
+                  {dataItem?.bio.slice(1, 100)}...
+                </p>
+                <div className="flex justify-between items-center mt-4">
+                  <span className="text-lg font-semibold text-green-600">
+                    Price : ${dataItem?.price}
+                  </span>
+                  <span className="text-sm text-gray-500">
+                    Enrolled: {dataItem?.enroll}
+                  </span>
+                </div>
+                <NavLink
+                  to={`/allclass/classdetails/${dataItem?._id}`}
+                  className={"card-actions justify-center w-full"}
+                >
+                  <button className="text-white btn bg-indigo-700 hover:bg-indigo-400 w-full ">
+                    Enroll
+                  </button>
+                </NavLink>
               </div>
-              <NavLink
-                to={`/allclass/classdetails/${dataItem?._id}`}
-                className={"card-actions justify-center w-full"}
-              >
-                <button className="text-white btn bg-indigo-700 hover:bg-indigo-400 w-full ">
-                  Enroll
-                </button>
-              </NavLink>
             </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   );
 };
 
--- src/Components/Home/Card/Cards.jsx
+++ src/Components/Home/Card/Cards.jsx
@@ -11,6 +11,7 @@ import { Autoplay, Pagination } from "swiper/modules";
 
 const Cards = () => {
   const [sortedData, setSortedData] = useState([]);
+  const [sorted, setSorted] = useState("");
 
   const [data, refetch] = useHomePageAll("/allclass");
 
@@ -19,16 +20,37 @@ const Cards = () => {
       const i = item.status === "approves";
       return i;
     });
-    const sorting = filterData.sort((a, b) => b.enroll - a.enroll);
-    const confrimData = sorting.slice(0, 6);
+    if (sorted == "asc") {
+      const ascSort = filterData.sort((a, b) => a.enroll - b.enroll);
+      const ascConfrimData = ascSort.slice(0, 6);
+      setSortedData(ascConfrimData);
+      return;
+    } else if (sorted == "desc") {
+      const descSort = filterData.sort((a, b) => b.enroll - a.enroll);
+      const descConfrimData = descSort.slice(0, 6);
+      setSortedData(descConfrimData);
+      return;
+    }
+    const confrimData = filterData.slice(0, 6);
     setSortedData(confrimData);
-  }, [data]);
+  }, [data, sorted]);
 
   return (
     <div className="">
-      <h2 className="text-3xl font-bold text-center my-7 text-gray-800">
+      <h2 className="text-3xl font-bold text-center my-3 text-gray-800">
         Popular Courses
       </h2>
+      <div className="flex gap-3 items-center justify-center mb-6">
+        <h4 className="text-xl font-semibold text-nowrap">Sort By Enroll:</h4>
+        <select
+          onChange={(e) => setSorted(e.target.value)}
+          className="select select-bordered w-full max-w-xs"
+        >
+          <option value={""}>Default Data</option>
+          <option value="asc">Ascending By Enroll</option>
+          <option value="desc">Descending By Enroll</option>
+        </select>
+      </div>
       {/* grid md:grid-cols-2 lg:grid-cols-3 gap-5 */}
       <div className="grid sm:grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-7">
         {/* <Swiper
